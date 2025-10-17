0dceaa023f37767e8623de925d71f6494b4415cf
Richyy <richyy@Richyys-MacBook-Pro.local>
2025-10-17 11:34:05 -0400

    Add React Project

---
+++ src/Navbar.jsx
@@ -0,0 +1,15 @@
+export default function Navbar() {
+  return (
+    <>
+      <div className="header">
+        <div>
+          <h2>Flight Agent</h2>
+        </div>
+        <div>
+          <h3>Sign In</h3>
+          <h3>Create Account</h3>
+        </div>
+      </div>
+    </>
+  );
+}
